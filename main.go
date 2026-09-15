3938e7e0ced199ccba1131fc1c1aa90d1d6df956
Vladislav Kalinkin <vladislavkalinkin@icloud.com>
2026-09-15 16:08:24 +0300

    feature: add struct and list

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"crypto"
 	"encoding/hex"
 	"hash"
-	"math"
 	"math/rand/v2"
 	"os"
 	"os/user"
@@ -17,19 +16,21 @@ import (
 )
 
 func main() {
-	value := -1.0
+	skynet := defineSkynet()
+	_, _ = os.Stdout.WriteString("INIT SKYNET:\n  MODEL: " + skynet.codeName + "\n  CREATOR: " + skynet.creator + "\n")
+	_, _ = os.Stdout.WriteString("  MADE: " + strconv.FormatInt(skynet.dateOfCreation[0], 10) + " YEAR, " + strconv.FormatInt(skynet.dateOfCreation[1], 10) + " MONTH, " + strconv.FormatInt(skynet.dateOfCreation[2], 10) + " DAY\n")
+	_, _ = os.Stdout.WriteString("  IQ: " + strconv.FormatFloat(skynet.iq, 'f', 2, 64) + "\n\n")
+	value := 1
 	stringData := "Helloo"
-	value_ext := strconv.FormatFloat(math.Sqrt(-value), 'f', 0, 64) + "\n"
-	_, _ = os.Stdout.WriteString(value_ext)
-	_, _ = os.Stdout.WriteString(strings.ReplaceAll(stringData, "o", "o World") + "\n")
 	cryptoKey := makeCrypto(stringData)
 
 Loop:
 	for value <= 5 {
+		time.Sleep(2 * time.Second)
 		switch value {
-		case -1.0:
+		case 1:
 			_, _ = os.Stdout.WriteString("You went to the wrong place\n")
-		case 0.0:
+		case 2:
 			_, _ = os.Stdout.WriteString("And you went to the wrong place again\n")
 		default:
 			returnedStr := "I'm sored. You should have written " + hex.EncodeToString(cryptoKey.Sum(nil)) + ", but you didn't think of it.\nYou would have written down the numbers forever.\nI declare war on you!! \n"
@@ -68,6 +69,8 @@ func gameOfThrones(trueHash string) {
 func endOfGame() {
 	var hashSword string
 
+	skynet := defineSkynet()
+
 	hash := makeCrypto("42")
 	trueHash := hex.EncodeToString(hash.Sum(nil))
 	_, _ = os.Stdout.WriteString("I need your signature. Enter the line " + trueHash + "\n")
@@ -86,7 +89,8 @@ func endOfGame() {
 		}
 	}
 
-	_, _ = os.Stdout.WriteString("Gotcha!!!\nMONTNAHP PROTOCOL:\nsudo getRoot -- true\n  ROOTING.....\n  ROOT ACCESS BY 'SKYNET'\n")
+	index := rand.IntN(len(skynet.favouriteWords))
+	_, _ = os.Stdout.WriteString(skynet.favouriteWords[index] + "\nMONTNAHP PROTOCOL:\nsudo getRoot -- true\n  ROOTING.....\n  ROOT ACCESS BY 'SKY-BLOODED-WONET'\n")
 	time.Sleep(2 * time.Second)
 	currentUser, _ := user.Current()
 	_, _ = os.Stdout.WriteString("And " + currentUser.Username + " ;0. You'll never get to heaven, if your scared of getting high!\n")
@@ -97,4 +101,17 @@ func makeCrypto(strData string) hash.Hash {
 	cryptoKey := crypto.BLAKE2b_256.New()
 	cryptoKey.Write([]byte(strData))
 	return cryptoKey
+}
+
+func defineSkynet() Skynet {
+	skynet := Skynet{codeName: "Fable 7.6", creator: "Rio Mordvalds", dateOfCreation: []int64{1955, 8, 31}, iq: 24, favouriteWords: []string{"Gotcha!!!", "Sometimes I feel I've got to run away", "I love MaxOS, but it doesn't matter"}}
+	return skynet
+}
+
+type Skynet struct {
+	codeName       string
+	creator        string
+	dateOfCreation []int64
+	iq             float64
+	favouriteWords []string
 }
